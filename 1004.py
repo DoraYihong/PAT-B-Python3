e1ecc89b7772716e655a944a64e396727dcf046d
Dora Yihong <694612150@qq.com>
2019-11-25 15:08:21 +0800

    1004

--- 1004.py
+++ 1004.py
@@ -1,2 +1,14 @@
-for i in range(5, 9):
-    print(i)
+num = int(input())
+name = []
+sid = []
+mark = []
+for i in range(num):
+    s = input()
+    x = s.split(" ", 2)
+    name.append(x[0])
+    sid.append(x[1])
+    mark.append(int(x[2]))  # 注意把字符串转化为数字处理
+i = mark.index(max(mark))
+print(name[i]+" "+sid[i])
+i = mark.index(min(mark))
+print(name[i]+" "+sid[i])
