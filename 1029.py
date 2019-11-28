4e5cb7bbe7cce92c99e2b98405efb15903b3018b
Dora Yihong <694612150@qq.com>
2019-11-28 20:19:16 +0800

    1029

---
+++ 1029.py
@@ -0,0 +1,15 @@
+str1 = list(input())
+str2 = input()
+for i in str2:
+    while i in str1:
+        str1.remove(i)
+
+for i in range(len(str1)):
+    if str1[i].islower:
+        str1[i] = str1[i].upper()
+
+str3 = []
+for x in str1:
+    if x not in str3:
+        str3.append(x)
+print(''.join(i for i in str3))
